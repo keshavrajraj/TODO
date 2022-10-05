b1c8cce9c9239dbb060f1855485b991df13dfb92
keshavrajraj <39114797+keshavrajraj@users.noreply.github.com>
2022-10-05 23:40:24 +0100

    Add files via upload

---
+++ src/main/java/net/java/todoapp/model/username.java
@@ -0,0 +1,20 @@
+
+package net.java.todoapp.model;
+
+import java.io.Serializable;
+
+public class username implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String username;
+
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	
+}
